bf8013415269c060063740b2fe38fba2c54a4382
foxihd <120084738+foxihd@users.noreply.github.com>
2026-04-29 12:51:58 +0700

    add more dotfiles autodetect

--- usr/local/sbin/live-usb-storage.sh
+++ usr/local/sbin/live-usb-storage.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
-LIVE=$HOME/Live-usb-storage
+LIVE=${HOME}/Live-usb-storage
 
-if [ -d "$LIVE/.config/dconf" ] ; then
-    rm -R $HOME/.config/dconf && ln -s $LIVE/.config/dconf $HOME/.config/dconf
+# low-level gnome configuration
+if [ -d "${LIVE}/.config/dconf" ] ; then
+    rm -R "${HOME}/.config/dconf" && ln -s "${LIVE}/.config/dconf" "${HOME}/.config/dconf"
 fi
 
-if [ -d "$LIVE/.local/share/gnome-shell" ] ; then
-    rm -R $HOME/.local/share/gnome-shell && ln -s $LIVE/.local/share/gnome-shell $HOME/.local/share/gnome-shell
+# desktop files
+if [ -d "${LIVE}/.local/share/application" ] ; then
+    rm -R "${HOME}/.local/share/application" && ln -s "${LIVE}/.local/share/application" "${HOME}/.local/share/application"
 fi
 
-if [ -d "$LIVE/.local/share/application" ] ; then
-    rm -R $HOME/.local/share/application && ln -s $LIVE/.local/share/application $HOME/.local/share/application
+# gnome-shell configuration
+if [ -d "${LIVE}/.local/share/gnome-shell" ] ; then
+    rm -R "${HOME}/.local/share/gnome-shell" && ln -s "${LIVE}/.local/share/gnome-shell" "${HOME}/.local/share/gnome-shell"
 fi
 
-if [ -d "$LIVE/.vscode-oss" ] ; then
-    ln -s $LIVE/.vscode-oss $HOME/
-    if [ -e "$LIVE/.vscode-oss/settings.json" ] ; then
-        mkdir -p $HOME/.config/VSCodium/User && ln -s $LIVE/.vscode-oss/settings.json $HOME/.config/VSCodium/User/settings.json
+# codium settings & extension
+if [ -d "${LIVE}/.vscode-oss" ] ; then
+    ln -s "${LIVE}"/.vscode-oss "${HOME}/"
+    cfgs=( "keybindings.json" "settings.json" ) 
+    for cfg in "${cfgs[@]}"; do
+        if [ -e "${LIVE}/.vscode-oss/${cfg}" ] ; then
+            mkdir -p "${HOME}/.config/VSCodium/User" && ln -s "${LIVE}/.vscode-oss/${cfg}" "${HOME}/.config/VSCodium/User/${cfg}"
+        fi
+    done
+fi
+
+# other dotfiles
+dotfiles=( ".aliases" ".bash_aliases" ".bash_profile" ".bash_prompt" ".bashrc" ".curlrc" ".editorconfig" ".gitattributes" ".gitconfig" ".profile" ".vimrc" ".wgetrc" ".zshrc" ) 
+for dotfile in "${dotfiles[@]}"; do
+    if [ -e "${LIVE}/dotfiles/${dotfile}" ] ; then
+        rm "${HOME}/${dotfile}" && ln -sf "${LIVE}/dotfiles/${dotfile}" "${HOME}/${dotfile}"
     fi
-fi
+done
